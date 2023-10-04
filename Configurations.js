d10dd6bd5617f4c1ae193e19a2f0e9493c56b9c6
knightsqua <139598324+knightsqua@users.noreply.github.com>
2023-10-04 09:50:54 +0100

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "2347017498108";   // You can replace this number with yours //
 }
 
 
